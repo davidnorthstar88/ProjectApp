f996b10699571fcdcb499838c555bca1046f4f8a
David Northstar II <davidnorthstar88@gmail.com>
2026-07-25 12:01:33 -0400

    Update Program.cs

--- EventEase/Program.cs
+++ EventEase/Program.cs
@@ -1,18 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers(); // if you still need APIs
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor(); // required for hosted WASM
+builder.Services.AddRazorComponents()
+    .AddInteractiveServer();
 
 var app = builder.Build();
 
-app.UseBlazorFrameworkFiles();   // <-- critical
 app.UseStaticFiles();
-
 app.UseRouting();
 
-app.MapRazorPages();
-app.MapControllers();
-app.MapFallbackToFile("index.html"); // <-- SPA routing
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapFallbackToPage("/_Host");
 
 app.Run();
