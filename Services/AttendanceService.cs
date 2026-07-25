9a760bb6d78f82d6f55a06ad158ff24d01f150c4
David Northstar II <davidnorthstar88@gmail.com>
2026-07-25 08:39:26 -0400

    Create AttendanceService.cs

---
+++ Services/AttendanceService.cs
@@ -0,0 +1,35 @@
+using EventEase.Models;
+
+namespace EventEase.Services;
+
+public class AttendanceService
+{
+    private readonly List<AttendanceRecord> _records = new();
+
+    public IEnumerable<AttendanceRecord> GetAll() => _records;
+
+    public void MarkAttendance(int registrationId, string name, bool present)
+    {
+        var existing = _records.FirstOrDefault(r => r.RegistrationId == registrationId);
+
+        if (existing is null)
+        {
+            _records.Add(new AttendanceRecord
+            {
+                RegistrationId = registrationId,
+                AttendeeName = name,
+                Present = present
+            });
+        }
+        else
+        {
+            existing.Present = present;
+        }
+    }
+
+    public int TotalPresent =>
+        _records.Count(r => r.Present);
+
+    public int TotalRegistered =>
+        _records.Count();
+}
