827330cb658306d63e2d99b099bcca2a6eaee085
David Northstar II <davidnorthstar88@gmail.com>
2026-07-25 08:38:19 -0400

    Create RegistrationService.cs

---
+++ Services/RegistrationService.cs
@@ -0,0 +1,22 @@
+using EventEase.Models;
+
+namespace EventEase.Services;
+
+public class RegistrationService
+{
+    private readonly List<Registration> _registrations = new();
+    private int _nextId = 1;
+
+    public Registration Add(Registration registration)
+    {
+        registration.Id = _nextId++;
+        _registrations.Add(registration);
+        return registration;
+    }
+
+    public IEnumerable<Registration> GetByEvent(int eventId) =>
+        _registrations.Where(r => r.EventId == eventId);
+
+    public Registration? GetById(int id) =>
+        _registrations.FirstOrDefault(r => r.Id == id);
+}
