095c2469385b11fcd031581ab5b210666779cc14
David Northstar II <davidnorthstar88@gmail.com>
2026-07-25 08:38:53 -0400

    Create UserSessionService.cs

---
+++ Services/UserSessionService.cs
@@ -0,0 +1,17 @@
+using EventEase.Models;
+
+namespace EventEase.Services;
+
+public class UserSessionService
+{
+    public UserSession Session { get; } = new();
+
+    public void SetUser(string name) =>
+        Session.CurrentUserName = name;
+
+    public void SetLastViewedEvent(int eventId) =>
+        Session.LastViewedEventId = eventId;
+
+    public void IncrementRegistrations() =>
+        Session.RegistrationsCount++;
+}
