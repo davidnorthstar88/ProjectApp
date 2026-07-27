e568cb9b2a048e3ba37dc5b3560bde67bd70f7a8
David Northstar II <davidnorthstar88@gmail.com>
2026-07-27 16:05:08 +0000

    commit

--- ProjectApp/Program.cs
+++ ProjectApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ProjectApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
