dc4afe0aaa649e4de33147d65b9a92ccd08b238a
David Northstar II <davidnorthstar88@gmail.com>
2026-07-28 16:00:24 +0000

    commit

--- ProjectApp/obj/Release/net8.0/ProjectApp.AssemblyInfo.cs
+++ ProjectApp/obj/Release/net8.0/ProjectApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f339aeb58c58292bbf5851e35ca59fac63639090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+634c5d0cd054300893b8e8faaf26cf1996a98ab2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
