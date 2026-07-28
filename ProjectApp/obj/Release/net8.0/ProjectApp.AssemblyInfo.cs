dcc1383a966fa87624a42fb30ace3ae1fe3490cf
David Northstar II <davidnorthstar88@gmail.com>
2026-07-28 17:16:57 +0000

    Deploy ProjectApp

--- ProjectApp/obj/Release/net8.0/ProjectApp.AssemblyInfo.cs
+++ ProjectApp/obj/Release/net8.0/ProjectApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7a49433b1a6afea40c167203ca3ef365e749a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f3e72619d751853c20b95a1073939634e7c3ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
