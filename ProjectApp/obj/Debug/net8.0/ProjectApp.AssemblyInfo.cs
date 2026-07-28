47ff65ef4d14040afecf28621504814a4d6a34d8
David Northstar II <davidnorthstar88@gmail.com>
2026-07-28 14:56:09 +0000

    commit

--- ProjectApp/obj/Debug/net8.0/ProjectApp.AssemblyInfo.cs
+++ ProjectApp/obj/Debug/net8.0/ProjectApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa7d767b80e8bb609ee74458073b0bcb33464a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e389c4601c5066636a9df5ecfd86b6d29ed9aaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
