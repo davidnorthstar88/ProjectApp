879915ac5e70816db0dbdb63b4b9e6f3de158982
David Northstar II <davidnorthstar88@gmail.com>
2026-07-28 15:00:07 +0000

    commit

---
+++ ProjectApp/docs/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+// General site-wide JS helpers
+
+console.log("EventEase site.js loaded");
+
+// Smooth scroll helper
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
