57f83dd0b2860c6cba4a76fe432ffb57b691212f
David Northstar II <davidnorthstar88@gmail.com>
2026-07-29 13:21:50 +0000

    Remove stray project folder from gh-pages

--- ProjectApp/js/site.js
+++
@@ -1,8 +0,0 @@
-// General site-wide JS helpers
-
-console.log("EventEase site.js loaded");
-
-// Smooth scroll helper
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-}
