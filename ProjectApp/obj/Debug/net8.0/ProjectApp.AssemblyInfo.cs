3dbdbd3af0075b8842e78ad45936550db8f2526a
David Northstar II <davidnorthstar88@gmail.com>
2026-07-29 14:15:27 +0000

    Publish EventEase to GitHub Pages

--- ProjectApp/obj/Debug/net8.0/ProjectApp.AssemblyInfo.cs
+++ ProjectApp/obj/Debug/net8.0/ProjectApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dd3724d90b8761b2c6c01b7696ba71a45aeebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3dd23e44f989416bd917a08ff660ded719917b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
